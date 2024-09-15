b600da01f9f7403415fb2da6ab25034bca47f76f
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-09-15 16:21:19 +0000

    [add]admin-item-edit、controlleとprivate入力

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,6 +12,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
-    
+    @item = Item.find(params[:id])
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
   end
 end
