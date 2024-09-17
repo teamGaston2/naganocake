515121a2e0362b89d28b14bfc033abcf7b2d522d
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-09-17 08:14:15 +0000

    view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,4 +11,21 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+    
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
 end
