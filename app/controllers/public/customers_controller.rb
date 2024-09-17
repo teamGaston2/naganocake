7878e3bba54c9440e228ca8b3fbb4ab522569fe3
jasmine2434 <echan2434@gmail.com>
2024-09-17 06:21:40 +0000

    customer3

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,34 +1,34 @@
 class Public::CustomersController < ApplicationController
-  # def show
-  #   @customer = Customer.find(params[:id])
-  # end
-  
-  # def edit
-  #   @customer = Customer.find(params[:id])
-  #   if @customer == current_user
-  #     render :edit
-  #   else
-  #     redirect_to customer_path(current_user)
-  #   end
-  # end
-  
-  # def update
-  #   @customer = Customer.find(params[:id])
-  #     if @customer.update(customer_params)
-  #       flash[:notice] = "変更を保存しました。"
-  #       redirect_to customer_path
-  #     else
-  #       render :edit
-  #     end
-  # end
+  def show
+    @customer = current_customer
+  end
 
-  # def unsubscribe
-  # end
-  
-  # def customer_params
-  #   params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
-  #     :email, :post_code, :address, :telephone_number)
-  # end
-  
+  def edit
+    @customer = Customer.find(params[:id])
+    if @customer == current_customer
+      render :edit
+    else
+      redirect_to customer_path(current_user)
+    end
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+      if @customer.update(customer_params)
+        flash[:notice] = "変更を保存しました。"
+        redirect_to customer_path
+      else
+        render :edit
+      end
+  end
+
+  def unsubscribe
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
+      :email, :post_code, :address, :telephone_number)
+  end
+
 
 end
