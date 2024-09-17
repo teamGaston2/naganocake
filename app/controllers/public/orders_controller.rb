f111fbbf2c610c67bc16c1b98df95d0aef8fe067
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-09-17 12:23:00 +0000

    New address

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,14 +14,16 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
-    
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.first_name + current_customer.last_name
+    # 下記条件分岐が必要
+    # byebug
+    # @address = Address.find(params[:order][:address_id])
+    # @order.postal_code = @address.postal_code
+    # @order.address = @address.address
+    # @order.name = @address.name
+
+    # @order.postal_code = current_customer.postal_code
+    # @order.address = current_customer.address
+    # @order.name = current_customer.first_name + current_customer.last_name
   end
 
   private
