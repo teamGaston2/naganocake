c454465094747101c53f219dfdb36aabc76123f0
jasmine2434 <echan2434@gmail.com>
2024-09-17 02:52:18 +0000

    customers1

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,34 @@
 class Public::CustomersController < ApplicationController
-  def show
-    
-  end
+  # def show
+  #   @customer = Customer.find(params[:id])
+  # end
+  
+  # def edit
+  #   @customer = Customer.find(params[:id])
+  #   if @customer == current_user
+  #     render :edit
+  #   else
+  #     redirect_to customer_path(current_user)
+  #   end
+  # end
+  
+  # def update
+  #   @customer = Customer.find(params[:id])
+  #     if @customer.update(customer_params)
+  #       flash[:notice] = "変更を保存しました。"
+  #       redirect_to customer_path
+  #     else
+  #       render :edit
+  #     end
+  # end
+
+  # def unsubscribe
+  # end
+  
+  # def customer_params
+  #   params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
+  #     :email, :post_code, :address, :telephone_number)
+  # end
+  
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,13 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password, presence: true, length: { minimum: 6 }
+
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+
+  def full_name_kana
+    self.last_name_kana + " " + self.first_name_kana
+  end
+
 end
