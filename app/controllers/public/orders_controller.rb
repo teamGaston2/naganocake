0cf764844b5d65dc3d3f254c9c18ea22ff58cdf6
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-09-18 04:59:14 +0000

    button

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -47,6 +47,9 @@ class Public::OrdersController < ApplicationController
     @order_details = OrderDetail.all
     @shipping = 800
     @total = 0
+    @payment_methods = params[:order][:payment_method]
+    @payment_method_jp = I18n.t("activerecord.attributes.order.payment_method.#{@payment_method}")
+    # @addressee = Address.find(params[:id])
   end
 
   private
