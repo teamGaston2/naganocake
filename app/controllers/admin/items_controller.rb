d5f8e6e8a40deb45d223d0da959624d71733eb9a
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-09-18 17:21:36 +0000

    show途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -16,7 +16,7 @@ class Admin::ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -114,8 +114,8 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
     t.integer "order_id"
     t.integer "item_id"
     t.integer "price"
-    t.integer "amountexit"
-    t.integer "making_status", default: 0
+    t.integer "amount"
+    t.integer "making_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -129,7 +129,7 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
     t.integer "shipping_cost"
     t.integer "total_payment"
     t.integer "payment_method"
-    t.integer "order_status", default: 0
+    t.integer "order_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
