1aab4304dadecbac2951da8cb44727bd36171518
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-09-18 13:22:03 +0000

    genre edit

--- db/migrate/20240914083250_create_order_details.rb
+++ db/migrate/20240914083250_create_order_details.rb
@@ -5,7 +5,7 @@ class CreateOrderDetails < ActiveRecord::Migration[6.1]
       t.integer :order_id
       t.integer :item_id
       t.integer :price
-      t.integer :amountexit
+      t.integer :amount
       t.integer :making_status
 
       t.timestamps
