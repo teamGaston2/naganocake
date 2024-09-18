87151a3ded3ab3ddd5b859492e5058bf94b87ad8
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-09-18 11:57:53 +0000

    [注文ステータスと製作ステータスの連携完了]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,19 +7,14 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    if @order.update(order_params)
-       flash[:notice] = "注文ステータスを変更しました"
-       redirect_to admin_order_path(@order)
-    else
-       flash[:notice] = "注文ステータス変更が失敗しました"
-       redirect_to admin_order_path(@order)
+    @order.update(order_status: params[:order][:order_status])
+    @order_details = @order.order_details
+    if params[:order][:order_status] == "payment_confirmation"
+      @order_details.update(making_status: "waiting_production")
     end
+    
+  flash[:notice] = "注文ステータスを変更しました"
+  redirect_to admin_order_path(@order)
   end
-  
-private
 
-  def order_params
-    params.require(:order).permit(:order_status)
-  end
-  
 end
