3fbe76f599b017458b1f850de358e8d445327ea9
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-09-19 16:53:30 +0000

    last

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    # @genres = Genre.all ジャンルが追加できるようになったらこっち
-    @genres = ['ケーキ', '焼き菓子', 'プリン', 'キャンディ']
+    @genres = Genre.all
   end
 
   def about
