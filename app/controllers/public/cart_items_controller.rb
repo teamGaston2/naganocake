1ba49bcb3c72f237ee16606000c9de051923cfba
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-09-19 11:14:03 +0000

    index_and_show

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,13 @@
 class Public::CartItemsController < ApplicationController
   def index
-    
+
   end
 
   def new
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,5 +7,8 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @items = Item.all
+    # @genres = Genre.all
+    @genres = ['ケーキ', '焼き菓子', 'プリン', 'キャンディ']
   end
 end
