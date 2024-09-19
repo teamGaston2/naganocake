e4ddd94eab8bc79e9e2757c2f5d4a08bac25ff2d
jasmine2434 <echan2434@gmail.com>
2024-09-19 12:29:04 +0000

    cart11

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
   end
 
   create_table "addresses", force: :cascade do |t|
+    t.integer "address_id"
     t.integer "customer_id"
     t.string "postal_code"
     t.string "address"
@@ -62,6 +63,7 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
   end
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer "care_item_id"
     t.integer "customer_id"
     t.integer "item_id"
     t.integer "amount"
@@ -80,15 +82,24 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
+    t.integer "genre_id"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", force: :cascade do |t|
+    t.integer "item_id"
     t.integer "genre_id"
     t.string "name"
     t.text "introduction"
@@ -99,16 +110,18 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
   end
 
   create_table "order_details", force: :cascade do |t|
+    t.integer "order_detail_id"
     t.integer "order_id"
     t.integer "item_id"
     t.integer "price"
     t.integer "amount"
-    t.integer "making_status", default: 0
+    t.integer "making_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "order_id"
     t.integer "customer_id"
     t.string "postal_code"
     t.string "address"
@@ -116,7 +129,7 @@ ActiveRecord::Schema.define(version: 2024_09_17_092755) do
     t.integer "shipping_cost"
     t.integer "total_payment"
     t.integer "payment_method"
-    t.integer "order_status", default: 0
+    t.integer "order_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
