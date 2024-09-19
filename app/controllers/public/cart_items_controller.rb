2f98cde484f02683767dd929b3d4b05178e39513
jasmine2434 <echan2434@gmail.com>
2024-09-19 06:50:47 +0000

    cart7

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,15 +5,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = Cart_item.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item.item_id = cart_item_params[:item_id]
       if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
-        cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+        @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
         cart_item.amount += params[:cart_item][:amount].to_i
-        cart_item.update(amount: cart_item.amount)
+        cart_item.save
         redirect_to cart_items_path
       else
+        @cart_item = Cart_item.new(cart_item_params)
+        cart_item.customer_id = current_customer.id
+        cart_item.item_id = cart_item_params[:item_id]
         cart_item.save
         redirect_to cart_items_path
       end
@@ -33,11 +33,10 @@ class Public::CartItemsController < ApplicationController
 
   def destroy_all
     CartItem.destroy_all
-   redirect_to cart_items_path
+    redirect_to cart_items_path
   end
 
   private
-
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
